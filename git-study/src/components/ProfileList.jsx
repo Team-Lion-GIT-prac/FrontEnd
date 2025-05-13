3a502851284164c8714f4865d7489fce93240fd6
ysys3535 <yoonseo3139@gmail.com>
2025-05-13 19:01:51 +0900

    hi

--- git-study/src/components/ProfileList.jsx
+++ git-study/src/components/ProfileList.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import ProfileCard from './ProfileCard';
 import './ProfileList.css';
 import star from '../assets/shootingstar.jpg'
+import dog from '../assets/멍멍.png'
+
 
 const profiles = [
+  {
+    name: '윤서',
+    bio: '안녕하세용 최윤서입니다.^^',
+    image: dog,
+  },
   {
     name: '유성',
     bio: 'AI에 부쩍 관심이 생긴 개발자 입니다. :D',
