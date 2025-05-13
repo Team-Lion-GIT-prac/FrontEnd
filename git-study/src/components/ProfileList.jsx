fb7214491a996526cc030d9b39f2d7989e9c2d37
KimYujin22 <ksyskim1312@naver.com>
2025-05-13 19:02:03 +0900

    yoojin-profile update

--- git-study/src/components/ProfileList.jsx
+++ git-study/src/components/ProfileList.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ProfileCard from './ProfileCard';
 import './ProfileList.css';
 import star from '../assets/shootingstar.jpg'
+import lion from '../assets/babylion.jpg'
 
 const profiles = [
+  {
+    name: '유진',
+    bio: '개발만 못하는 개발자 입니다! ',
+    image: lion,
+  },
   {
     name: '유성',
     bio: 'AI에 부쩍 관심이 생긴 개발자 입니다. :D',
