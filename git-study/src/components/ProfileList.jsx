4cdac39d0ce5cca54f92e5aa9493509f478d8a0a
cykimbb <cykimbb@gmail.com>
2025-05-13 19:01:53 +0900

    첫 번째 커밋!

--- git-study/src/components/ProfileList.jsx
+++ git-study/src/components/ProfileList.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ProfileCard from './ProfileCard';
 import './ProfileList.css';
 import star from '../assets/shootingstar.jpg'
+import cat from '../assets/bongbong.jpg'
 
-const profiles = [
+const profiles = [ {
+    name: '채윤',
+    bio: '흠',
+    image: cat,
+  },
   {
     name: '유성',
     bio: 'AI에 부쩍 관심이 생긴 개발자 입니다. :D',
