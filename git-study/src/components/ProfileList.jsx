f548f83911dc85ab615254872a7b44ec6faeaf2a
seohyun <westhyun1107@gmail.com>
2025-05-13 19:02:04 +0900

    seohyun-profile_1st

--- git-study/src/components/ProfileList.jsx
+++ git-study/src/components/ProfileList.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ProfileCard from './ProfileCard';
 import './ProfileList.css';
 import star from '../assets/shootingstar.jpg'
+import cloud from '../assets/cloud.jpg'
 
 const profiles = [
+  {
+    name: '서현',
+    bio: '‧˚₊*̥(* ⁰̷̴͈꒳⁰̷̴͈ )‧˚₊*̥',
+    image: cloud,
+  },
   {
     name: '유성',
     bio: 'AI에 부쩍 관심이 생긴 개발자 입니다. :D',
